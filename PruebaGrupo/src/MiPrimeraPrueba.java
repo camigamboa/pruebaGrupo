60e57219e6cd03a6e93347fe45f8c7d0cde103f0
acami <acami@DESKTOP-I2AJPO5.mshome.net>
2023-03-21 15:48:01 -0300

    Cree mi primera clase

---
+++ PruebaGrupo/src/MiPrimeraPrueba.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class MiPrimeraPrueba {
+
+	public static void main(String[] args) {
+				
+		JOptionPane.showMessageDialog(null, "Mi primer commit");
+	}
+
+}
